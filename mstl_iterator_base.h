bd9f98587c9572d15dccbd1f0e681f526f9a5c6e
yxc <136000550@qq.com>
2024-07-16 22:28:32 +0800

    关闭部分接口

--- mstl_iterator_base.h
+++ mstl_iterator_base.h
@@ -6,6 +6,8 @@
 
 NAMESPACE_MSTL
 
+
+//TODO: 删除纯虚函数接口后，要在随机访问容器里改一下override签名 
 /* 奇异递归模板模式(Curiously Recurring Template Pattern) */
 template <typename Derived, typename U>
 class iterator_base {
@@ -18,13 +20,13 @@ public:
     virtual Derived& operator++() = 0;
     virtual Derived operator++(int) = 0;
 
-    virtual Derived& operator--() = 0;
-    virtual Derived operator--(int) = 0;
+    // virtual Derived& operator--() {};
+    // virtual Derived operator--(int){};
 
-    virtual Derived operator+(difference_type offset) const = 0;
-    virtual Derived operator-(difference_type offset) const = 0;
+    // virtual Derived operator+(difference_type offset) const {};
+    // virtual Derived operator-(difference_type offset) const {};
 
-    virtual difference_type operator-(const Derived& other) const = 0;
+    // virtual difference_type operator-(const Derived& other) const {};
 
     virtual bool operator==(const Derived& other) const = 0;
     virtual bool operator!=(const Derived& other) const = 0;
